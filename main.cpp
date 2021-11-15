99a27fa053438c270c3d746cd2c05a8458bab0f4
Leonardo Hernandez <leonardograna2@gmail.com>
2021-11-14 20:50:55 -0600

    Captura nuestro nombre y un hola mundo

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	char nombre[10];
+	
+	cout<<"ingresa tu nombre: ";
+	cin>>nombre;
+	
+	cout<<nombre<<endl;
+	cout<<"Hola mundo";
+	
+	
+	
+	
+	return 0;	
+	
+}
